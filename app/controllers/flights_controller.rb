9c00ad772d32c818741b2ae407b0e4123599a6a2
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-06-13 10:57:28 +0200

    Setting the search action && the _form_search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,4 +8,14 @@ class FlightsController < ApplicationController
         @flight = Flight.new
     end
 
+    def search
+        origin = params[:origin]
+        destination = params[:destination]
+        departure_date = params[:departure_date]
+        
+        @flights = Flight.where(origin_city: origin, destination_city: destination, departure_date: departure_date)
+        
+        # Pass @flights to the view for rendering
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'pages#home'
+  root 'flights#index'
   resources :flights, only: [:index, :show]
 
 end
